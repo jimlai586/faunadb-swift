ccf5449a9b5d5ce022a654eacb41533b36d92171
jimlai <swift2931@gmail.com>
2019-07-07 14:04:31 +0800

    Update JsonTests.swift

--- Tests/FaunaDBTests/JsonTests.swift
+++ Tests/FaunaDBTests/JsonTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import FaunaDB
 
 class JsonTests: XCTestCase {
-    let client = FaunaDB.Client(secret: "fnADQgHyHoACCaSGtYgf38JtFENEJsAehfQZmn7E")
+    let client = FaunaDB.Client(secret: "client secret")
     var exp = XCTestExpectation(description: "async")
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
